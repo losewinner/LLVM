8b80cffc1331b156b8cc294b6f90d92ea1ec7713
Krystian Stasiowski <sdkrystian@gmail.com>
2024-09-12 09:42:05 -0400

    [FOLD] fix dependent overloaded arrow

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -8006,7 +8006,7 @@ ExprResult Sema::ActOnStartCXXMemberReference(Scope *S, Expr *Base,
       if (IsDependent) {
         // BuildOverloadedArrowExpr sets IsDependent to indicate that we need
         // to build a dependent overloaded arrow expression.
-        assert(BaseType->isDependentType());
+        assert(Base->isTypeDependent());
         BaseType = Context.DependentTy;
         break;
       }
