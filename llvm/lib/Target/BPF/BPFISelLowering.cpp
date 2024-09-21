43f8a400ef1333e923475ce115f190b0e01ca411
Peilin Ye <yepeilin@google.com>
2024-09-21 04:00:40 +0000

    [BPF] Improve error message for seq_cst atomic load and store
    Sequentially consistent (seq_cst) atomic load and store are not
    supported yet for BPF.  Right now, calling __atomic_{load,store}{,_n}()
    with __ATOMIC_SEQ_CST will cause an error:
    
      $ cat bar.c
      int foo(int *ptr) { return __atomic_load_n(ptr, __ATOMIC_SEQ_CST); }
      $ clang --target=bpf -mcpu=v4 -c bar.c > /dev/null
      fatal error: error in backend: Cannot select: t8: i32,ch = AtomicLoad<(load seq_cst (s32) from %ir.0)> t7:1, t7
      ...
    
    Which isn't very useful.  Just like commit 379d90884807 ("BPF: provide
    better error message for unsupported atomic operations"), make it
    generate an error message saying that the requested operation isn't
    supported, before triggering that "fatal error":
    
      $ clang --target=bpf -mcpu=v4 -c bar.c > /dev/null
      bar.c:1:5: error: sequentially consistent (seq_cst) atomic load/store is not supported
        1 | int foo(int *ptr) { return __atomic_load_n(ptr, __ATOMIC_SEQ_CST); }
          |     ^
      ...

--- llvm/lib/Target/BPF/BPFISelLowering.cpp
+++ llvm/lib/Target/BPF/BPFISelLowering.cpp
@@ -93,6 +93,11 @@ BPFTargetLowering::BPFTargetLowering(const TargetMachine &TM,
     setOperationAction(ISD::ATOMIC_CMP_SWAP_WITH_SUCCESS, VT, Custom);
   }
 
+  for (auto VT : {MVT::i32, MVT::i64}) {
+    setOperationAction(ISD::ATOMIC_LOAD, VT, Custom);
+    setOperationAction(ISD::ATOMIC_STORE, VT, Custom);
+  }
+
   for (auto VT : { MVT::i32, MVT::i64 }) {
     if (VT == MVT::i32 && !STI.getHasAlu32())
       continue;
@@ -291,6 +296,9 @@ void BPFTargetLowering::ReplaceNodeResults(
     else
       Msg = "unsupported atomic operation, please use 64 bit version";
     break;
+  case ISD::ATOMIC_LOAD:
+  case ISD::ATOMIC_STORE:
+    return;
   }
 
   SDLoc DL(N);
@@ -316,6 +324,9 @@ SDValue BPFTargetLowering::LowerOperation(SDValue Op, SelectionDAG &DAG) const {
     return LowerSDIVSREM(Op, DAG);
   case ISD::DYNAMIC_STACKALLOC:
     return LowerDYNAMIC_STACKALLOC(Op, DAG);
+  case ISD::ATOMIC_LOAD:
+  case ISD::ATOMIC_STORE:
+    return LowerATOMIC_LOAD_STORE(Op, DAG);
   }
 }
 
@@ -701,6 +712,20 @@ SDValue BPFTargetLowering::LowerSELECT_CC(SDValue Op, SelectionDAG &DAG) const {
   SDValue Ops[] = {LHS, RHS, TargetCC, TrueV, FalseV};
 
   return DAG.getNode(BPFISD::SELECT_CC, DL, VTs, Ops);
+}
+
+SDValue BPFTargetLowering::LowerATOMIC_LOAD_STORE(SDValue Op,
+                                                  SelectionDAG &DAG) const {
+  SDNode *N = Op.getNode();
+  SDLoc DL(N);
+
+  if (cast<AtomicSDNode>(N)->getMergedOrdering() ==
+      AtomicOrdering::SequentiallyConsistent)
+    fail(DL, DAG,
+         "sequentially consistent (seq_cst) "
+         "atomic load/store is not supported");
+
+  return Op;
 }
 
 const char *BPFTargetLowering::getTargetNodeName(unsigned Opcode) const {
