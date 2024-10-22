851ee7a3fd4f1ae294f3e0baaf0944caeadb7d05
Greg Clayton <clayborg@gmail.com>
2024-10-22 01:16:40 -0700

    Make the target's SectionLoadList private.
    Lots of code around LLDB was directly accessing the target's section load list. This NFC patch makes the section load list private to the Target class can access it, but everyone else now uses accessor functions. This allows us to control the resolving of addresses and will allow for functionality in LLDB which can lazily resolve addresses in JIT plug-ins with a future patch.

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -5066,3 +5066,17 @@ llvm::json::Value
 Target::ReportStatistics(const lldb_private::StatisticsOptions &options) {
   return m_stats.ToJSON(*this, options);
 }
+
+bool Target::SectionLoadListIsEmpty() const {
+  return const_cast<Target *>(this)->GetSectionLoadList().IsEmpty();
+}
+
+lldb::addr_t Target::GetSectionLoadAddress(const lldb::SectionSP &section_sp) {
+  return GetSectionLoadList().GetSectionLoadAddress(section_sp);
+}
+
+void Target::ClearSectionLoadList() { GetSectionLoadList().Clear(); }
+
+void Target::DumpSectionLoadList(Stream &s) {
+  GetSectionLoadList().Dump(s, this);
+}
