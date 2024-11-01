aced10b670cc86ad7ed8a0d9a3e35d99a033d8ac
Sam Tebbs <samuel.tebbs@arm.com>
2024-11-01 16:01:27 +0000

    Clean up invalid partial reduction removal

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -8830,27 +8830,23 @@ void VPRecipeBuilder::removeInvalidScaledReductionExitInstrs() {
   // extends are intended to be lowered along with the reduction itself.
 
   // Build up a set of partial reduction bin ops for efficient use checking
-  SmallSet<Instruction *, 4> PartialReductionBinOps;
-  for (auto It : ScaledReductionExitInstrs) {
-    if (It.second.BinOp)
-      PartialReductionBinOps.insert(It.second.BinOp);
+  SmallSet<User *, 4> PartialReductionBinOps;
+  for (const auto &[_, Chain] : ScaledReductionExitInstrs) {
+    if (Chain.BinOp)
+      PartialReductionBinOps.insert(Chain.BinOp);
   }
 
   auto ExtendIsOnlyUsedByPartialReductions =
-      [PartialReductionBinOps](Instruction *Extend) {
-        for (auto *Use : Extend->users()) {
-          Instruction *UseInstr = dyn_cast<Instruction>(Use);
-          if (!PartialReductionBinOps.contains(UseInstr))
-            return false;
-        }
-        return true;
+      [&PartialReductionBinOps](Instruction *Extend) {
+        return all_of(Extend->users(), [&](const User *U) {
+          return PartialReductionBinOps.contains(U);
+        });
       };
 
   // Check if each use of a chain's two extends is a partial reduction
   // and remove those that have non-partial reduction users
   SmallSet<Instruction *, 4> PartialReductionsToRemove;
-  for (auto It : ScaledReductionExitInstrs) {
-    PartialReductionChain Chain = It.second;
+  for (const auto &[_, Chain] : ScaledReductionExitInstrs) {
     if (!ExtendIsOnlyUsedByPartialReductions(Chain.ExtendA) ||
         !ExtendIsOnlyUsedByPartialReductions(Chain.ExtendB))
       PartialReductionsToRemove.insert(Chain.Reduction);
