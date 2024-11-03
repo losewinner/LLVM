7bfbd4f74287e709a157044ffcd2ffe8459fcc15
Noah Goldstein <goldstein.w.n@gmail.com>
2024-11-03 09:12:27 -0600

    Fixup spelling mistake

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -589,7 +589,7 @@ static void breakSelfRecursivePHI(const Use *U, const PHINode *PHI,
   CtxIOut = PHI->getIncomingBlock(*U)->getTerminator();
   Value *V;
   // If the Use is a select of this phi, compute analysis on other arm to be
-  // recusion.
+  // recursion.
   // TODO: Min/Max
   if (match(ValOut, m_Select(m_Value(), m_Specific(PHI), m_Value(V))) ||
       match(ValOut, m_Select(m_Value(), m_Value(V), m_Specific(PHI)))) {
