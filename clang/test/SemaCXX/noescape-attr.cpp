12a608aa8e6844a171200849e30eedd79cdfb4d8
Gabor Horvath <gaborh@apple.com>
2024-11-22 16:19:35 +0000

    [Clang] Permit noescape on non-pointer types
    In modern C++ we often use span, string_view or other view objects
    instead of raw pointers. This PR opens the door to mark those noescape.
    This can be useful to document the API contracts, for interop with
    memory safe languages like Swift or Rust, and possibly in the future to
    implement take this into account in codegen.
    
    The PR also adds a feature. The goal is to help avoiding warnings when
    the code is compiler by earlier versions of clang that does not permit
    this attribute on non-pointer types.

--- clang/test/SemaCXX/noescape-attr.cpp
+++ clang/test/SemaCXX/noescape-attr.cpp
@@ -1,7 +1,12 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 
+// expected-no-diagnostics
+
 template<typename T>
 void test1(T __attribute__((noescape)) arr, int size);
 
-// expected-warning@+1 {{'noescape' attribute only applies to pointer arguments}}
 void test2(int __attribute__((noescape)) arr, int size);
+
+#if !__has_feature(attribute_noescape_nonpointer)
+  #error "attribute_noescape_nonpointer should be supported"
+#endif
