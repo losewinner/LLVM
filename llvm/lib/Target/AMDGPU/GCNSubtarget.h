f5010817c83a515b54b3f55848b9ad834bee7d08
Austin Kerbow <Austin.Kerbow@amd.com>
2024-12-01 14:02:25 -0800

    Diagnostic error and move LDSKernelId.
    Use diagnostic error. Unify preload kernarg SGPR accounting and move
    LDSKernelId to UserSGPRInfo so that synthetic SGPRs can be properly
    tracked when calculating the number of available registers for preloads.

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -1676,11 +1676,13 @@ public:
 
   bool hasPrivateSegmentSize() const { return PrivateSegmentSize; }
 
+  bool hasLDSKernelId() const { return LDSKernelId; }
+
   unsigned getNumKernargPreloadSGPRs() const { return NumKernargPreloadSGPRs; }
 
   unsigned getNumUsedUserSGPRs() const { return NumUsedUserSGPRs; }
 
-  unsigned getNumFreeUserSGPRs();
+  unsigned getNumFreeKernargPreloadSGPRs();
 
   void allocKernargPreloadSGPRs(unsigned NumSGPRs);
 
@@ -1692,11 +1694,12 @@ public:
     KernargSegmentPtrID = 4,
     DispatchIdID = 5,
     FlatScratchInitID = 6,
-    PrivateSegmentSizeID = 7
+    PrivateSegmentSizeID = 7,
+    LDSKernelIdID = 8
   };
 
   // Returns the size in number of SGPRs for preload user SGPR field.
-  static unsigned getNumUserSGPRForField(UserSGPRID ID) {
+  static constexpr unsigned getNumUserSGPRForField(UserSGPRID ID) {
     switch (ID) {
     case ImplicitBufferPtrID:
       return 2;
@@ -1713,6 +1716,8 @@ public:
     case FlatScratchInitID:
       return 2;
     case PrivateSegmentSizeID:
+      return 1;
+    case LDSKernelIdID:
       return 1;
     }
     llvm_unreachable("Unknown UserSGPRID.");
@@ -1742,9 +1747,13 @@ private:
 
   bool PrivateSegmentSize = false;
 
+  bool LDSKernelId = false;
+
   unsigned NumKernargPreloadSGPRs = 0;
 
   unsigned NumUsedUserSGPRs = 0;
+
+  unsigned NumSyntheticSGPRs = 0;
 };
 
 } // end namespace llvm
